77771d47470a281f9755e6345802db7d4211e376
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-05-07 18:42:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+![Binaryhood](Logo/BinaryhoodLogo.png)
+
+# Flask Chatbot with Langchain and OpenAI GPT
+
+This is a chatbot application built with Flask, Langchain, and OpenAI's GPT 3.5 turbo(ChatGPT) model. The chatbot can access the internet using DuckDuckGo and Wikipedia APIs, and can also run Python programs using Python REPL.
+
+## Features
+
+- Chat with the chatbot using natural language.
+- The chatbot will get answers to questions using one or more of the following tools from the Python repl, DuckDuckGo and Wikipedia.
+- Run Python programs using Python REPL.
+- User-friendly interface.
+
+## Installation
+
+To install and run the application make sure you have Python and git installed on your computer then follow these steps:
+
+1. Clone the repository to your local machine using `git clone <this repo>` on your terminal.
+2. Install the required dependencies using `pip install -r requirements.txt`.
+3. Create an OpenAI API key and add it to your environment variables.
+4. Run the application using `python main.py`.
+5. Access the application in your web browser at `http://localhost:5000`.
+
+## Usage
+
+To use the chatbot, follow these steps:
+
+1. Type a message to the chatbot in the chat box and hit Enter.
+2. Wait for the chatbot to respond.
+
+Example chatbot commands:
+
+- `Hello` - The chatbot will respond with a greeting.
+- `What is the capital of France?` - The chatbot will search the internet for the answer.
+- `search Who invented the internet?` - The chatbot will search DuckDuckGo for the answer.
+- `wiki Who is Elon Musk?` - The chatbot will search Wikipedia for the answer.
+- `python print("Hello, world!")` - The chatbot will run the Python program and return the output.
+
+## Demo
+
+[Click here](https://flask-chatbot-langchain.herokuapp.com/) to view a live demo of the application.
+
+## Contributing
+
+If you would like to contribute to the project, please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ main.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request #, jsonify
+from langchain.agents import Tool
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.utilities import PythonREPL
+from langchain.utilities import WikipediaAPIWrapper
+import openai
+import config
+
+#get Openapi key
+api_key= config.DevelopmentConfig.OPENAI_KEY
+
+openai.api_key = api_key
+
+
+#Create tools that will be used
+
+#Duck Duck go search engine
+search = DuckDuckGoSearchRun()
+Duck=Tool( name = "Current Search",  func=search.run,    description="useful for when you need to answer questions about current events or the current state of the world. the input to this should be a single search term.")
+
+#Python repl
+python_repl = PythonREPL()
+Python = Tool(      name = "python repl",  func=python_repl.run,description="useful for when you need to use python to answer a question. You should input python code")
+
+#Wikipedia
+wikipedia = WikipediaAPIWrapper()
+wikipedia_tool = Tool(    name='wikipedia',    func= wikipedia.run, description="Useful for when you need to look up a topic, country or person on wikipedia")
+
+# append all tools to tools
+tools = []
+tools.append(Duck)
+tools.append(Python)
+tools.append(wikipedia_tool)
+
+
+
+#create memory for the chatbot
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# create agent
+llm=ChatOpenAI(temperature=0, openai_api_key=api_key)
+agent_chain = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory, )#return_intermediate_steps=True
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+   ans= agent_chain.run(text)
+   try:
+     answer = ans.replace('\n', '<br>')
+   except:
+     answer = "opps no response"
+   print(answer)
+   return answer
+        
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
