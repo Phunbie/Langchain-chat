2326dd8bc669595b2d5489f662b21ec1ba0516c0
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-05-07 19:30:03 +0000

    Change config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
-    OPENAI_KEY = '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'
+    OPENAI_KEY = ''
 
 config = {
     'development': DevelopmentConfig,
